5b7024fc1af0c1831117d7ae84e4f6263104f00f
entertain_c <a2569942117@gmail.com>
2023-04-06 00:08:18 +0800

    feat: add Singleton function demo

--- Singleton/Singleton.js
+++ Singleton/Singleton.js
@@ -1,34 +1,63 @@
 // Singleton pattern
-class Singleton{
-  static instance = new Singleton();
+
+// Class Demo
+// class Singleton{
+//   static instance = new Singleton();
   
-  constructor() {
-    if (Singleton.instance) {
-      return Singleton.instance;
-    }
-    Singleton.instance = this;
+//   constructor() {
+//     if (Singleton.instance) {
+//       return Singleton.instance;
+//     }
+//     Singleton.instance = this;
+//   }
+
+//   getInstance() {
+//     return this.instance;
+//   }
+
+//   getName() {
+//     return this.testMap;
+//   }
+
+//   addName(name) {
+//     if (!this.testMap) {
+//       this.testMap = [];
+//     }
+//     this.testMap.push(name);
+//   }
+// }
+
+// const instance1 = new Singleton();
+// instance1.addName('instance1');
+// console.log(instance1.getName()); // instance1
+// const instance2 = new Singleton();
+// console.log(instance2.getName()); // instance1
+
+// ---------------------------------------------------------------
+// Function Demo
+// Singleton pattern
+const Singleton = (function () {
+  let instance;
+
+  function createInstance() {
+    const object = new Object({ name: 'Singleton' });
+    return object;
   }
 
-  getInstance() {
-    return Singleton.instance;
-  }
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    },
+  };
+})();
 
-  getName() {
-    return this.testMap;
-  }
+const instance1 = Singleton.getInstance();
+const instance2 = Singleton.getInstance();
 
-  addName(name) {
-    if (!this.testMap) {
-      this.testMap = [];
-    }
-    this.testMap.push(name);
-  }
-}
-
-const instance1 = new Singleton();
-instance1.addName('instance1');
-console.log(instance1.getName()); // instance1
-const instance2 = new Singleton();
-console.log(instance2.getName()); // instance1
+console.log(instance1); // true
 
 // enter node Singleton/Singleton.js in Terminal to run the code
+
